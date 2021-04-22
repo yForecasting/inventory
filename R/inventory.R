0ee029f5a226b0d9ebe8faf22bce83cdf4466310
yForecasting <yves.sagaert@gmail.com>
2021-04-22 16:28:05 +0200

    package inventory v 0.0.0.9

---
+++ R/inventory.R
@@ -0,0 +1,27 @@
+#' inventory function
+#'
+#' Creates simulation from a given forecast
+#'
+#' This function creates a inventory simulation based on a forecast and a
+#' given inventory policy to evaluate the average on-hand inventory and the
+#' service level.
+#'
+#' @param forecast A data frame or matrix which represents the forecast
+#' distribution
+#' @param h The forecast horizon
+#' @param ipolicy The inventory policy. "MTS" (Make to Stock) by default
+#'
+#' @author Yves R. Sagaert
+#'
+#' import forecast
+#'
+#' @return A list of average on-hand inventory and service levels.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    inventory(forecastdata, h=12, ipolicy="MTS")
+#' }
+inventory <- function(forecast, h, ipolicy="MTS"){
+  return("inventory simulation here")
+}
---
+++ R/inventory_package.R
@@ -0,0 +1,10 @@
+#' inventory: A package for performing inventory simulations for forecast
+#' evaluation
+#'
+#' This package provides functionality for a inventory simulations via a Monte
+#' Carlo setup for robust forecast evaluation.
+#'
+#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
+#' @docType package
+#' @name inventory
+"_PACKAGE"
