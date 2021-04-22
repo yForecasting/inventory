3de54a7665b4943bec446070af9bf8b7e5d12e98
yForecasting <yves.sagaert@gmail.com>
2021-04-22 21:38:38 +0200

    Add dependencies Mcomp and forecast, add example

--- R/inventory.R
+++ R/inventory.R
@@ -6,7 +6,7 @@
 #' given inventory policy to evaluate the average on-hand inventory and the
 #' service level.
 #'
-#' @param forecast A data frame or matrix which represents the forecast
+#' @param forecast A forecast object, data frame or matrix which represents the forecast
 #' distribution
 #' @param h The forecast horizon
 #' @param ipolicy The inventory policy. "MTS" (Make to Stock) by default
@@ -14,15 +14,18 @@
 #' @author Yves R. Sagaert
 #' @author Sarah Van der Auweraer
 #'
-#' import forecast
+#' @import forecast
+#' @import Mcomp
 #'
 #' @return A list of average on-hand inventory and service levels.
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#'    inventory(forecastdata, h=12, ipolicy="MTS")
-#' }
+#'
+#'    ets.model <- forecast::ets(Mcomp::M3[[2000]]$x)
+#'    ets.forecast <- forecast::forecast(ets.model,Mcomp::M3[[2000]]$h)
+#'    inventory(forecast=ets.forecast, h=12, ipolicy="MTS")
+#'
 inventory <- function(forecast, h, ipolicy="MTS"){
   return("inventory simulation here")
 }
